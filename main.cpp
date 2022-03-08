00284c35563db5639d95fff9b23594456c0daa3d
Tram Nguyen <trmnguyen2608@gmail.com>
2022-03-08 12:53:50 -0700

    SRTF commit

--- main.cpp
+++ main.cpp
@@ -128,26 +128,28 @@ void SRTF(vector<processInfo> info){
     int completeP = 0, t =0, minmReTime = INT_MAX;
     int shortestP = 0, finishTime;
     bool flag = false;
-    int waitTime[info.size()], turnaroundTime[info.size()], finishtime[info.size()];
+    int waitTime[info.size()], turnaroundTime[info.size()], finishtimeArr[info.size()];
     for (int i = 0; i < info.size(); i++)
         reBurstTime[i] = info[i].burstTime;
     // Process until all processes gets
     // completed
     while (completeP < info.size()) {
 
-        // Find process with minimum
-        // remaining time among the
-        // processes that arrives till the
-        // current time`
+        // Find process with the smallest remaining time among the
+        // processes that arrives up to the current time
         for (int j = 0; j < info.size(); j++) {
-            if ((info[j].arrivalTime <= t) &&
-                (reBurstTime[j] < minmReTime) && reBurstTime[j] > 0) {
-                minmReTime = reBurstTime[j];
-                shortestP = j;
-                flag = true;
+            //if the arrival time of the process is before the current time and
+            // has remaining burst time less than the smallest remaining time (but making sure it is positive time)
+            if ((info[j].arrivalTime <= t) && (reBurstTime[j] < minmReTime) && reBurstTime[j] > 0) {
+                minmReTime = reBurstTime[j];    //update the smallest remaining time to the new process
+                shortestP = j;                  //change the process with the smallest burst time
+                flag = true;                    //set the flag to true
+                //i think the increment of context switching would be here cuz we are updating the new process
+                //and preempt the current process??
             }
         }
-
+        //if we cannot find any other process that has the smallest remaining burst time at the current time,
+        //we increment the time
         if (flag == false) {
             t++;
             continue;
@@ -158,26 +160,24 @@ void SRTF(vector<processInfo> info){
 
         // Update minimum
         minmReTime = reBurstTime[shortestP];
-        if (minmReTime == 0)
-            minmReTime = INT_MAX;
+        if (minmReTime == 0)    //if the smallest remaining time is 0, the process has finished executed
+            minmReTime = INT_MAX;   //change the smallest remaining time so that it can get the next process
+                                    //with the smallest burst time
 
-        // If a process gets completely
-        // executed
+        // If a process gets completely executed
         if (reBurstTime[shortestP] == 0) {
-
-            // Increment complete
+            // Increment  the number of completed process
             completeP++;
             flag = false;
-
             // Find finish time of current
             // process
             finishTime = t + 1;
-
+            finishtimeArr[shortestP] = finishTime;  //store the process's finish time into the array
             // Calculate waiting time
-            waitTime[shortestP] = finishTime -
-                           info[shortestP].burstTime -
-                           info[shortestP].arrivalTime;
-
+            waitTime[shortestP] = finishTime - info[shortestP].burstTime - info[shortestP].arrivalTime;
+            // If waiting time for a process is in negative
+            // that means it is already in the ready queue
+            // before CPU becomes idle so its waiting time is 0
             if (waitTime[shortestP] < 0)
                 waitTime[shortestP] = 0;
         }

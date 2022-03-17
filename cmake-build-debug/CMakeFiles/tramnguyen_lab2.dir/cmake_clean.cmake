299304520eb417a665485619d18bb97774231ec7
Tram Nguyen <trmnguyen2608@gmail.com>
2022-03-17 17:10:56 -0600

    scheduler done

---
+++ cmake-build-debug/CMakeFiles/tramnguyen_lab2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tramnguyen_lab2.dir/main.cpp.o"
+  "CMakeFiles/tramnguyen_lab2.dir/main.cpp.o.d"
+  "tramnguyen_lab2"
+  "tramnguyen_lab2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tramnguyen_lab2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/tramnguyen_lab2.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for tramnguyen_lab2.
+# This may be replaced when dependencies are built.
